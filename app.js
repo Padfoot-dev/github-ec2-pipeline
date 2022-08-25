8953aa425eb1f508233a1584dc1c763569276dc9
ABISHEK <abish102001@gmail.com>
2022-08-25 08:52:22 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+
+app.get("/",(req,res)=> {
+    res.send("Welcome to the express app!")
+})
+
+app.get("/products",(req,res)=> {
+
+    res.json({
+        "product-id" : "101",
+        "product-name":"product - 1",
+        "porduct-price" : 101
+    })
+})
+
+app.listen(port , ()=> {
+    console.log(`app listening on port ${port}`)
+})
---
+++ scripts/application_stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Stopping existing node servers
+echo "Stoppping any existing node servers"
+pkill node
